5c448275a2b29194681277d8085f85f02fd416c9
Keneriya <kenuts217@gmail.com>
2025-08-20 23:24:39 +0300

    init commit;

--- ToDoList/src/main/java/com/example/ToDoList/Controller/Public.java
+++ ToDoList/src/main/java/com/example/ToDoList/Controller/Public.java
@@ -1,4 +1,5 @@
 package com.example.ToDoList.Controller;
 
 public class Public {
+
 }
---
+++ ToDoList/src/main/java/com/example/ToDoList/configuration/WebConfig.java
@@ -0,0 +1,22 @@
+package com.example.ToDoList.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins("http://localhost:5173") // React frontend URL
+                        .allowedMethods("GET","POST","DELETE","PUT");
+            }
+        };
+    }
+}
