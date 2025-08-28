55927e1d4a0d8d64c29d4b9f791d233e16760781
Keneriya <kenuts217@gmail.com>
2025-08-28 10:44:22 +0300

    init commit;

---
+++ ToDoList/src/main/java/com/example/ToDoList/configuration/JwtUtils.java
@@ -0,0 +1,39 @@
+package com.example.ToDoList.configuration;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtUtils {
+    private final String jwtSecret = "mySecretKeyForJwtTokenMustBeLongEnough12345"; // Replace with env var
+    private final long jwtExpirationMs = 86400000; // 1 day
+
+    private final Key key = Keys.hmacShaKeyFor(jwtSecret.getBytes());
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String getUsernameFromJwt(String token) {
+        return Jwts.parserBuilder().setSigningKey(key).build()
+                .parseClaimsJws(token).getBody().getSubject();
+    }
+
+    public boolean validateJwtToken(String authToken) {
+        try {
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(authToken);
+            return true;
+        } catch (JwtException e) {
+            return false;
+        }
+    }
+}
---
+++ ToDoList/src/main/java/com/example/ToDoList/configuration/SecurityConfig.java
@@ -0,0 +1,4 @@
+package com.example.ToDoList.configuration;
+
+public class SecurityConfig {
+}
